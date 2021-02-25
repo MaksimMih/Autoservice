b53f303af8a5f61770170b023ce244ce7e63af7b
MaksimMih <vip.peta.1998@mail.ru>
2021-02-25 09:56:23 +0300

    Добавьте файлы проекта.

---
+++ AutoMih/Core.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoMih
+{
+    class Core
+    {
+        public static mmihailovEntities DB = new mmihailovEntities();
+        
+    }
+
+}
